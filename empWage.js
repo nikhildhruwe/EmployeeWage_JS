6c9cf804180c9deb06ec5c9a835c7625d86bc394
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-02 21:43:52 +0530

    Refactored: Code by replacing if else with switch case.

--- empWage.js
+++ empWage.js
@@ -4,13 +4,19 @@ let EMP_RATE_PER_HOUR = 20;
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 
+let empHrs = 0
+let empWage = 0;
+
 let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = 0, empWage = 0;
-if (empCheck == IS_PART_TIME)
-    empHrs = 4;
-else if (empCheck == IS_FULL_TIME)
-    empHrs = 8;
-else
-    empHrs = 0;
+switch (empCheck) {
+	case IS_PART_TIME :
+  				empHrs = 4;
+				break;
+	case IS_FULL_TIME :
+  				empHrs = 8;
+				break;
+	default :
+    			empHrs = 0;
+}
 empWage = empHrs * EMP_RATE_PER_HOUR;
 console.log("Employee Wage = "+empWage);
